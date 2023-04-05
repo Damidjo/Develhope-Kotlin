f1ea8315292ff28318e9e70f5fa6946de7f08c73
Giovanni <giodamico98@gmail.com>
2023-04-05 14:41:05 +0200

    Ex1 upload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    for (i in 1..100 step 2) {
+        if (i < 50) {
+            printFirst(i)
+        } else {
+            printSecond(i)
+        }
+    }
 }
+
+fun printFirst(i : Int) {
+    println("Number below: $i")
+}
+
+fun printSecond(i : Int) {
+    println("Number above: $i")
+}
+
