2b5241b29fc1f415ab0d52bc37ee3d743c04c6b1
Giovanni <giodamico98@gmail.com>
2023-04-10 13:31:33 +0200

    ex 3 fix

--- src/main/kotlin/exercise3/Main.kt
+++ src/main/kotlin/exercise3/Main.kt
@@ -1,11 +1,11 @@
 package exercise3
  fun main() {
         val emailUser = User.Email("user@example.com", "password123")
-        val facebookUser = User.Facebook("123456789", "fb-token")
+        val facebookUser = User.Facebook("123456789", "password123")
         val googleUser = User.Google("user@gmail.com", null)
 
         println(emailUser.toPrintableString())    // Output: Email user with id user@example.com and email password123
-        println(facebookUser.toPrintableString()) // Output: Facebook user with id 123456789 and token fb-token
+        println(facebookUser.toPrintableString()) // Output: Facebook user with id 123456789
         println(googleUser.toPrintableString())   // Output: Google user with id user@gmail.com and password not set
     }
 
--- src/main/kotlin/exercise3/User.kt
+++ src/main/kotlin/exercise3/User.kt
@@ -1,21 +1,19 @@
 package exercise3
+//Gerarchia User
     sealed class User {
         abstract val id: String
-        abstract val passwordOrEmail: String?
+        abstract val passwordOEmail: String?
+//Subclasses
+        data class Email(override val id: String, override val passwordOEmail: String?) : User()
+        data class Facebook(override val id: String, override  val passwordOEmail: String?) : User()
 
-        data class Email(override val id: String, override val passwordOrEmail: String?) : User()
-        data class Facebook(override val id: String, val token: String) : User() {
-            override val passwordOrEmail: String?
-                get() = null
-        }
-
-        data class Google(override val id: String, override val passwordOrEmail: String?) : User()
-
+        data class Google(override val id: String, override val passwordOEmail: String?) : User()
+//F
         fun toPrintableString(): String {
             return when (this) {
-                is Email -> "Email user with id $id and email ${passwordOrEmail ?: "not set"}"
-                is Facebook -> "Facebook user with id $id and token $token"
-                is Google -> "Google user with id $id and password ${passwordOrEmail ?: "not set"}"
+                is Email -> "Email user with id $id and email ${passwordOEmail ?: "not set"}"
+                is Facebook -> "Facebook user with id $id and password $passwordOEmail"
+                is Google -> "Google user with id $id and password ${passwordOEmail ?: "not set"}"
             }
         }
     }
